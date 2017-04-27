5995c3130d488899ea2a0ca3aefa2105eb93a8a4
Riyan Pratama <riyan.pratama@outlook.com>
2017-04-27 10:00:50 +0700

    Initial Commit

---
+++ Brownies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Brownies
+//
+//  Created by Wisnu Riyan Pratama Putra on 4/27/17.
+//  Copyright © 2017 Wisnu Riyan Pratama Putra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Brownies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Brownies
+//
+//  Created by Wisnu Riyan Pratama Putra on 4/27/17.
+//  Copyright © 2017 Wisnu Riyan Pratama Putra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
